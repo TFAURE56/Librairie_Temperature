f50878e1ec004f599e300a99c68f02e7e766a0f3
TFAURE <thomas.fe2005@outlook.com>
2024-09-13 10:36:27 +0200

    1er retour valeur
    Premier code avec un retour de valeur

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "wiringPiI2C.h"
 using namespace std;
 
 int main() {
-	cout << "hello, world!" << endl;
+	// Ouvrir le capteur en 0x18 (addresse du capteru MCP9808)
+	int fd = wiringPiI2CSetup(0x18);
+
+	// Configurer en mode continue
+	wiringPiI2CWriteReg16(fd, 0x01, 0);
+
+	// Lire le registre de température
+	cout << "Température : " << wiringPiI2CReadReg16(fd, 0x05) << endl;
+
 }
